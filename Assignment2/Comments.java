dd7700e432ec7a4956bca7a5a7f5d75230ad6178
Atif Quamar <88545624+atifquamar07@users.noreply.github.com>
2021-10-18 16:46:25 +0530

    Add files via upload

---
+++ Assignment2/Comments.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class Comments {
+
+    static ArrayList<String> com = new ArrayList<>();
+    static ArrayList<String> who_wrote_com = new ArrayList<>();
+    static ArrayList<Date> date = new ArrayList<>();
+
+}
