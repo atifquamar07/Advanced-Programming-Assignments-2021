8f1d78fcdb6b994a9377e8e4a9995daec58da1ec
Atif Quamar <88545624+atifquamar07@users.noreply.github.com>
2021-10-19 22:18:47 +0530

    Delete Comments.java

--- Assignment2/Comments.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-
-public class Comments {
-
-    static ArrayList<String> com = new ArrayList<>();
-    static ArrayList<String> who_wrote_com = new ArrayList<>();
-    static ArrayList<Date> date = new ArrayList<>();
-
-}
